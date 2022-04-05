8fff458415e296ba1558af739568c435eb78d164
shawasty <shawasty@gmail.com>
2022-04-05 03:15:44 -0400

    worked on details of brewery

--- brewery-app/src/components/Brewery.js
+++ brewery-app/src/components/Brewery.js
@@ -1,26 +1,37 @@
 
 import { useParams } from "react-router-dom"
+import { useState, useEffect } from 'react';
+import { breweryData } from '../services/constants';
 import React from 'react';
 
-const Brewery = (props) => {
+const Brewery = () => {
+    const { name } = useParams();
+    const [breweries, setBreweries] = useState ({})
 
-    console.log('props', props)
-    const {brewery_id } = useParams();
-    console.log (brewery_id)
+    useEffect(()=>{
+        handlegetBrewery();
+     },[])
+     const handlegetBrewery = ()=>{
+        breweryData(name).then(res=>{
+            const brewery = res.filter(item=> item.name === name )
+             setBreweries(brewery[0]) 
+             console.log('name',brewery)
+           })}
 
-   const brewery = props[brewery_id];
+//    const brewery = props[id];
     return (
         <div className="brewery">
-            <ul key={props.id}>
-                 <li><span>NAME : </span>{props.name}</li>
-                 <li><span>TYPE : </span>{props.brewery_type}</li>
-                 <li><span>CITY : </span>{props.city}</li>
-                 <li><span>STATE : </span>{props.state}</li>
-                 <li><span>COUNTRY : </span>{props.country}</li>
-                 <li><span>PHONE : </span>{props.phone}</li>
-                 <li><span>WEBSITE : </span><a href="{props.website_url}">Visit Site</a></li>
+            {/* use tenary to make sure there is data to avoid the data from hanging */}
+           {breweries ? <ul key={breweries.id}>
+                 <li><span>NAME : </span>{breweries.name}</li>
+                 <li><span>TYPE : </span>{breweries.brewery_type}</li>
+                 <li><span>CITY : </span>{breweries.city}</li>
+                 <li><span>STATE : </span>{breweries.state}</li>
+                 <li><span>COUNTRY : </span>{breweries.country}</li>
+                 <li><span>PHONE : </span>{breweries.phone}</li> 
+                  <li><span>WEBSITE : </span><a href="{props.website_url}">Visit Site</a></li>
                    
-            </ul>
+            </ul>: 'Loading...'}
         </div>
     );
 };
--- brewery-app/src/components/BreweryCard.js
+++ brewery-app/src/components/BreweryCard.js
@@ -10,19 +10,8 @@ const BreweryCard = (props) => {
 
             <h2>{props.name}</h2>
 
-            {/* <ul >
-                 <li><span>NAME : </span>{props.name}</li>
-                 <li><span>TYPE : </span>{props.brewery_type}</li>
-                 <li><span>CITY : </span>{props.city}</li>
-                 <li><span>STATE : </span>{props.state}</li>
-                 <li><span>COUNTRY : </span>{props.country}</li>
-                 <li><span>PHONE : </span>{props.phone}</li>
-                 <li><span>WEBSITE : </span><a href="{props.website_url}">Visit Site</a></li>
-                   
-            </ul> */}
-
             {/* created a button to take us to individual brewery when clicked */}
-            <button onClick={()=>navigate(`/breweries/${props.id}`)}>See More Details</button>
+            <button onClick={()=>navigate(`/brewery/${props.name}`)}>See More Details</button>
  
         </div>
     );
