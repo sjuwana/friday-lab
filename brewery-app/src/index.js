fe7ff7967325c79396c294b730c84d9d28867922
sjuwana <98033493+sjuwana@users.noreply.github.com>
2022-04-10 18:02:38 -0400

    added REact Dom

--- brewery-app/src/index.js
+++ brewery-app/src/index.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom'
-import 'react-slideshow-image/dist/styles.css';
-
+import React from "react";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "react-slideshow-image/dist/styles.css";
+import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-       <App />
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
-
+  document.getElementById("root")
 );
-
-
