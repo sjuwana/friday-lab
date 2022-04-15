366b8eef73510de9119bc9e4acabc3cfb117eb92
sjuwana <98033493+sjuwana@users.noreply.github.com>
2022-04-15 00:09:40 -0400

    latest brewery update

--- brewery-app/src/components/NewsArticle.js
+++ brewery-app/src/components/NewsArticle.js
@@ -1,19 +1,18 @@
 import React from "react";
 
 function NewsArticle({ data }) {
-    return (
+  return (
     <div className="news">
-        <h1 className="news__title">JAGS' News</h1>
-        <p className="news__title">We keep you updated with the latest Brewery news, so you can always keep track through us with our daily JAGS' news!</p>
-    <p className="news__description">{data.description}</p>
-    <p className="news__author">{data.author}</p>
-    <p className="news__title">{data.title}</p>
-    <p className="news__data.publishedAt">{data.publishedAt}</p>
-   <div>
-    <a  href={data.url}>Link to the Story</a>
+      <h1 className="news__title">JAGS' News</h1>
+
+      <p className="news__description">{data.description}</p>
+      <p className="news__author">{data.author}</p>
+      <p className="news__title">{data.title}</p>
+      <p className="news__data.publishedAt">{data.publishedAt}</p>
+      <div>
+        <a href={data.url}>Link to the Story</a>
+      </div>
     </div>
-   </div>
-    );
-    }
+  );
+}
 export default NewsArticle;
-    
